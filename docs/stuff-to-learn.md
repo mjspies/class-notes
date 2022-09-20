54dba2f92632b8ed21f4691f970f729d4393783d
mario <mario_j_spies@progressive.com>
2022-09-20 00:24:54 -0400

    updating my notes

---
+++ docs/stuff-to-learn.md
@@ -0,0 +1,10 @@
+ # Stuff to Learn
+
+ - What the heck is MongoDb and how is it different from *SQL*
+ - Markdown (this syntax)
+    > [Cheat sheet](https://www.markdownguide.org/basic-syntax/#links)
+ - YAML (another markdown syntax)
+ - Command Prompt - using the command line.
+ - [Scott Hanselman YouTube](https://www.youtube.com/playlist?list=PL0M0zPgJ3HSesuPIObeUVQNbKqlw5U2Vr)
+ - Dan North -> CUPID (as opposed to SOLID)
+ - Jeremy Miller wrote a response to that as well
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,14 +37,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/mjspies/intro-to-programming-sept-2022',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/mjspies/intro-to-programming-sept-2022',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
